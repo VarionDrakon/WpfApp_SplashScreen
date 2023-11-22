3e05eff31aa0774db74b2a363578c4948ed080b5
vario <vario@WS-MSC-001>
2023-11-22 08:27:22 +0300

    Add project files.

---
+++ WpfApp_SplashScreen/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_SplashScreen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            SplashScreen splashScreen = new SplashScreen("Resources/splash screen.png");
+            splashScreen.Show(false, true);
+            InitializeComponent();
+            splashScreen.Close(TimeSpan.FromSeconds(3));
+        }
+    }
+}
